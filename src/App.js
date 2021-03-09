58c33ea3b0a96d7dbba86774fe8fc2e13286138a
Dan Nguyen <dannd496@gmail.com>
2021-03-09 18:12:28 +0700

    Buoi 1: Gioi thieu react, cau truc thu muc, dan layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Sidebar from "./components/Sidebar";
+// import Content from "./components/Content";
 
+import Baitap1 from "./Baitap1";
+// JSX - Javascript XML => Kết hợp javascript với HTML lại với nhau
+
+// Component App
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <Header />
+    //   <div className="d-flex">
+    //     <div className="w-25">
+    //       <Sidebar />
+    //     </div>
+    //     <div className="w-75">
+    //       <Content />
+    //     </div>
+    //   </div>
+    //   <Footer />
+    // </div>
+
+    <Baitap1 />
   );
 }
 
